8d281bf36acbb1700ef7c5f32d9f48f41f4bf859
Vitor Baptista <vitor@vitorbaptista.com>
2016-02-09 14:32:20 +0000

    Convert to ES6 and configure Mocha and ESLint

--- app.js
+++ app.js
@@ -1,30 +1,21 @@
-'use strict';
-
-var SwaggerHapi = require('swagger-hapi');
-var Hapi = require('hapi');
-var app = new Hapi.Server();
-
-module.exports = app; // for testing
-
-var config = {
-  appRoot: __dirname // required config
+const SwaggerHapi = require('swagger-hapi');
+const Hapi = require('hapi');
+const app = new Hapi.Server();
+const config = {
+  appRoot: __dirname, // required config
 };
 
-SwaggerHapi.create(config, function(err, swaggerHapi) {
+SwaggerHapi.create(config, (err, swaggerHapi) => {
+  const port = process.env.PORT || 10010;
   if (err) { throw err; }
 
-  var port = process.env.PORT || 10010;
-  app.connection({ port: port });
-  app.address = function() {
-    return { port: port };
-  };
+  app.connection({ port });
+  app.address = () => ({ port });
 
-  app.register(swaggerHapi.plugin, function(err) {
-    if (err) { return console.error('Failed to load plugin:', err); }
-    app.start(function() {
-      if (swaggerHapi.runner.swagger.paths['/hello']) {
-        console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-      }
-    });
+  app.register(swaggerHapi.plugin, (_err) => {
+    if (_err) { throw _err; }
+    app.start(() => undefined);
   });
 });
+
+module.exports = app; // for testing
