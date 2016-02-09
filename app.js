291e8a8ae9bf5e0ed633bd8245666157e87763bb
Vitor Baptista <vitor@vitorbaptista.com>
2016-02-09 12:47:54 +0000

    Initial skeleton for swagger hapi
    $ swagger project create opentrials-api

---
+++ app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var SwaggerHapi = require('swagger-hapi');
+var Hapi = require('hapi');
+var app = new Hapi.Server();
+
+module.exports = app; // for testing
+
+var config = {
+  appRoot: __dirname // required config
+};
+
+SwaggerHapi.create(config, function(err, swaggerHapi) {
+  if (err) { throw err; }
+
+  var port = process.env.PORT || 10010;
+  app.connection({ port: port });
+  app.address = function() {
+    return { port: port };
+  };
+
+  app.register(swaggerHapi.plugin, function(err) {
+    if (err) { return console.error('Failed to load plugin:', err); }
+    app.start(function() {
+      if (swaggerHapi.runner.swagger.paths['/hello']) {
+        console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+      }
+    });
+  });
+});
