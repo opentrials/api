e90c2b4e315fbb921aa61299fef19b0919524a1d
Vitor Baptista <vitor@vitorbaptista.com>
2016-02-10 18:19:13 +0000

    Rename "app.js" to "server.js"
    It's more consistent with other hapijs codebases

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const config = require('./config');
 const SwaggerHapi = require('swagger-hapi');
 const Hapi = require('hapi');
-const app = new Hapi.Server();
+const server = new Hapi.Server();
 
 SwaggerHapi.create(config.swaggerHapi, (err, swaggerHapi) => {
   const port = config.port;
@@ -9,16 +9,16 @@ SwaggerHapi.create(config.swaggerHapi, (err, swaggerHapi) => {
                    ...config.hapi.plugins];
   if (err) { throw err; }
 
-  app.connection({ port });
-  app.address = () => ({ port });
+  server.connection({ port });
+  server.address = () => ({ port });
 
 
-  app.register(plugins, (_err) => {
+  server.register(plugins, (_err) => {
     if (_err) { throw _err; }
-    app.start(() => {
-      console.info('Server started at', app.info.uri); // eslint-disable-line no-console
+    server.start(() => {
+      console.info('Server started at', server.info.uri); // eslint-disable-line no-console
     });
   });
 });
 
-module.exports = app; // for testing
+module.exports = server; // for testing
