8aad0c78e3842451f1099020ccf6ce8b4c90ee22
Vitor Baptista <vitor@vitorbaptista.com>
2016-03-07 19:35:15 +0000

    [#10] Remove /trials endpoint
    You can get the list of trials by using the /search endpoint without a query
    string.

--- test/api/controllers/trials.js
+++ test/api/controllers/trials.js
@@ -6,32 +6,6 @@ describe('Trials', () => {
 
   afterEach(clearDB)
 
-  describe('GET /v1/trials', () => {
-    it('returns empty list if there\'re no trials', () => (
-      server.inject('/v1/trials')
-        .then((response) => {
-          response.statusCode.should.equal(200);
-          JSON.parse(response.result).should.deepEqual([]);
-        })
-    ));
-
-    it('returns the list of trials', () => {
-      return fixtures.trialWithRelated()
-        .then((model) =>
-          server.inject('/v1/trials')
-            .then((response) => {
-              response.statusCode.should.equal(200);
-
-              const result = JSON.parse(response.result);
-              const expectedResult = model.toJSON();
-              expectedResult.registration_date = expectedResult.registration_date.toISOString()
-
-              result.should.deepEqual([expectedResult]);
-            })
-        );
-    });
-  });
-
   describe('GET /v1/trials/{id}', () => {
     it('returns 404 if there\'s no trial with the received ID', () => (
       server.inject('/v1/trials/foo')
