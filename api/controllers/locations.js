5fb51a1009c361ca5d66ba171ec56649a164f762
Vitor Baptista <vitor@vitorbaptista.com>
2016-03-17 12:18:40 +0000

    [#17] Throw errors for debugging purposes

--- api/controllers/locations.js
+++ api/controllers/locations.js
@@ -3,7 +3,8 @@ const Location = require('../models/location');
 function list(req, res) {
   return new Location().fetchAll()
     .catch((err) => {
-      // FIXME: We're ignoring errors for now, but we should at least log them.
+      res.finish();
+      throw err;
     })
     .then(res.json);
 }
