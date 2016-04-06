a983e0636eb647a3ec843f02d4a3d0fb62b5b181
roll <roll@post.agency>
2016-04-06 17:35:24 +0300

    added new fields to records migration

---
+++ migrations/20160406170526_add_fields_to_records.js
@@ -0,0 +1,25 @@
+exports.up = (knex) => {
+
+  const addFiels = knex.schema.table('records', (table) => {
+      table.text('reference').notNullable();
+      table.jsonb('extracted_data').notNullable();
+  });
+
+  return Promise.all([
+    addFiels,
+  ]);
+
+};
+
+exports.down = (knex) => {
+
+  const removeFields = knex.schema.table('records', (table) => {
+      table.dropColumn('reference');
+      table.dropColumn('extracted_data');
+  });
+
+  return Promise.all([
+    removeFields,
+  ]);
+
+};
