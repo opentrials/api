f5e15465440cd612938d2d0e7633a52ebee9c3cc
roll <roll@post.agency>
2016-04-07 09:14:43 +0300

    fixed minor migrations issues

--- migrations/20160406170526_add_fields_to_records.js
+++ migrations/20160406170526_add_fields_to_records.js
@@ -1,25 +1,23 @@
 exports.up = (knex) => {
-
   const addFields = knex.schema.table('records', (table) => {
-      table.text('reference').nullable();
-      table.jsonb('extracted_data').nullable();
+    table.text('reference').
+        nullable();
+    table.jsonb('extracted_data').
+        nullable();
   });
 
   return Promise.all([
     addFields,
   ]);
-
 };
 
 exports.down = (knex) => {
-
   const removeFields = knex.schema.table('records', (table) => {
-      table.dropColumn('reference');
-      table.dropColumn('extracted_data');
+    table.dropColumn('reference');
+    table.dropColumn('extracted_data');
   });
 
   return Promise.all([
     removeFields,
   ]);
-
 };
--- migrations/20160406175457_add_meta_fields_to_entities.js
+++ migrations/20160406175457_add_meta_fields_to_entities.js
@@ -1,50 +1,45 @@
-var tables = [
-    'interventions',
-    'locations',
-    'organisations',
-    'persons',
-    'problems',
-    'records',
-    'sources',
-    'trials',
-]
+const tableNames = [
+  'interventions',
+  'locations',
+  'organisations',
+  'persons',
+  'problems',
+  'records',
+  'sources',
+  'trials',
+];
 
 exports.up = (knex) => {
+  const operations = [];
 
-  var operations = []
-  tables.forEach((table) => {
-      var addTimestamps = knex.schema.table(table, (table) => {
-          table.timestamp('created_at').nullable();
-          table.timestamp('updated_at').nullable();
-          table.specificType('links', 'text[]').nullable();
-          table.specificType('facts', 'text[]').nullable();
-      });
-      var addLinks = knex.schema.raw(
-        'CREATE INDEX '+table+'_links_idx on '+table+' USING GIN(links)')
-      var addFacts = knex.schema.raw(
-        'CREATE INDEX '+table+'_facts_idx on '+table+' USING GIN(facts)')
-      operations.push(addTimestamps);
-      operations.push(addLinks);
-      operations.push(addFacts);
+  tableNames.forEach((tableName) => {
+    const operation = knex.schema.table(tableName, (table) => {
+      table.timestamps();
+      table.specificType('links', 'text[]').
+        nullable().
+        index(undefined, 'GIN');
+      table.specificType('facts', 'text[]').
+        nullable().
+        index(undefined, 'GIN');
+    });
+    operations.push(operation);
   });
 
   return Promise.all(operations);
-
 };
 
 exports.down = (knex) => {
+  const operations = [];
 
-  var operations = []
-  tables.forEach((table) => {
-      var removeFields = knex.schema.table(table, (table) => {
-          table.dropColumn('created_at');
-          table.dropColumn('updated_at');
-          table.dropColumn('links');
-          table.dropColumn('facts');
-      });
-      operations.push(removeFields);
+  tableNames.forEach((tableName) => {
+    const operation = knex.schema.table(tableName, (table) => {
+      table.dropColumn('created_at');
+      table.dropColumn('updated_at');
+      table.dropColumn('links');
+      table.dropColumn('facts');
+    });
+    operations.push(operation);
   });
 
   return Promise.all(operations);
-
 };
