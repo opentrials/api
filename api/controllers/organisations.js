bde02725ade4ee49705d0bfef3a9ee0bb33f16bd
Igor Borysyuk <borysyuk@qarea.com>
2016-04-22 20:08:21 +0300

    instance api functions have been added

---
+++ api/controllers/organisations.js
@@ -0,0 +1,23 @@
+const Organisation = require('../models/organisation');
+
+function getOrganisation(req, res) {
+  const id = req.swagger.params.id.value;
+
+  return new Organisation({ id: id }).fetch({ withRelated: Organisation.relatedModels })
+    .catch((err) => {
+      res.finish();
+      throw err;
+    })
+    .then((organisation) => {
+      if (organisation) {
+        res.json(organisation);
+      } else {
+        res.status(404);
+        res.finish();
+      }
+    });
+}
+
+module.exports = {
+  get: getOrganisation,
+}
