6929e0c3bc686d343b6a3ba68ceeb83d16be1ca5
roll <roll@post.agency>
2016-05-01 18:10:42 +0200

    improved source_id migration

--- migrations/20160501171307_add_source_id.js
+++ migrations/20160501171307_add_source_id.js
@@ -1,71 +1,31 @@
 'use strict';
 
+const tableNames = [
+  'interventions',
+  'locations',
+  'organisations',
+  'persons',
+  'problems',
+];
+
 exports.up = (knex, Promise) => {
-  const addSourceIdToProblems = knex.schema.table('problems', (table) => {
-    table.uuid('source_id')
-      .references('sources.id')
-      .nullable();
-  });
+  const operations = tableNames.map((tableName) => (
+    knex.schema.table(tableName, (table) => {
+      table.uuid('source_id')
+        .references('sources.id')
+        .nullable();
+    })
+  ));
 
-  const addSourceIdToInterventions = knex.schema.table('interventions', (table) => {
-    table.uuid('source_id')
-      .references('sources.id')
-      .nullable();
-  });
-
-  const addSourceIdToLocations = knex.schema.table('locations', (table) => {
-    table.uuid('source_id')
-      .references('sources.id')
-      .nullable();
-  });
-
-  const addSourceIdToOrganisations = knex.schema.table('organisations', (table) => {
-    table.uuid('source_id')
-      .references('sources.id')
-      .nullable();
-  });
-
-  const addSourceIdToPersons = knex.schema.table('persons', (table) => {
-    table.uuid('source_id')
-      .references('sources.id')
-      .nullable();
-  });
-
-  return Promise.all([
-    addSourceIdToProblems,
-    addSourceIdToInterventions,
-    addSourceIdToLocations,
-    addSourceIdToOrganisations,
-    addSourceIdToPersons,
-  ]);
+  return Promise.all(operations);
 };
 
 exports.down = (knex, Promise) => {
-  const removeSourceIdFromProblems = knex.schema.table('problems', (table) => {
-    table.dropColumn('source_id');
-  });
+  const operations = tableNames.map((tableName) => (
+    knex.schema.table(tableName, (table) => {
+      table.dropColumn('source_id');
+    })
+  ));
 
-  const removeSourceIdFromInterventions = knex.schema.table('interventions', (table) => {
-    table.dropColumn('source_id');
-  });
-
-  const removeSourceIdFromLocations = knex.schema.table('locations', (table) => {
-    table.dropColumn('source_id');
-  });
-
-  const removeSourceIdFromOrganisations = knex.schema.table('organisations', (table) => {
-    table.dropColumn('source_id');
-  });
-
-  const removeSourceIdFromPersons = knex.schema.table('persons', (table) => {
-    table.dropColumn('source_id');
-  });
-
-  return Promise.all([
-    removeSourceIdFromProblems,
-    removeSourceIdFromInterventions,
-    removeSourceIdFromLocations,
-    removeSourceIdFromOrganisations,
-    removeSourceIdFromPersons,
-  ]);
+  return Promise.all(operations);
 };
