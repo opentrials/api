977a1fa9396c8f5d61a0ca2cfbb57a7a346ef24d
roll <roll@post.agency>
2016-08-24 10:17:57 +0300

    removed records.source_trial_id (former primary_id)

--- test/factory.js
+++ test/factory.js
@@ -126,7 +126,6 @@ factory.define('record', Record, Object.assign({}, trialAttributes, {
   source_id: factory.assoc('source', 'id'),
   source_url: factory.sequence((n) => `http://source.com/trial/${n}`),
   source_data: JSON.stringify({}),
-  source_trial_id: factory.sequence((n) => `source_trial_id${n}`),
 }), {
   afterCreate: (record, attrs, callback) => {
     new Record({ id: record.id })
