ec678e44f6a5118560901a8356a51ee8800a0517
Georgiana Bere <bere.georgi@gmail.com>
2016-10-04 12:30:01 +0300

    Remove stats endpoint

--- test/api/controllers/stats.js
+++ test/api/controllers/stats.js
@@ -7,19 +7,12 @@ describe('Stats', () => {
 
   describe('GET /v1/stats', () => {
 
-    it('returns the Stats', () => {
+    it('returns 404', () => {
       return factory.create('trialWithRelated')
         .then(() => factory.create('record'))
         .then(() => server.inject('/v1/stats'))
         .then((response) => {
-          response.statusCode.should.equal(200);
-          const result = JSON.parse(response.result);
-
-          result.trialsCount.should.equal(2);
-          result.trialsPerSource.should.be.an.Array();
-          result.trialsPerYear.should.be.an.Array();
-          result.topLocations.should.be.an.Array();
-          result.sourcesLatestUpdatedDate.should.be.an.Array();
+          response.statusCode.should.equal(404);
         });
     })
   });
