4304835e06b7fab3caa62b7bdee76b503ba4508b
Vitor Baptista <vitor@vitorbaptista.com>
2016-11-14 16:25:00 +0200

    [#501] Add FDA Application and Approval to trial.documents
    opentrials/opentrials#501

--- tools/indexers/trials.js
+++ tools/indexers/trials.js
@@ -161,6 +161,96 @@ const trialMapping = {
     publication: {
       type: 'string',
     },
+    documents: {
+      properties: {
+        id: {
+          type: 'string',
+          index: 'not_analyzed',
+        },
+        name: {
+          type: 'string',
+        },
+        source_id: {
+          type: 'string',
+          index: 'not_analyzed',
+        },
+        source_url: {
+          type: 'string',
+          index: 'not_analyzed',
+        },
+        type: {
+          type: 'string',
+        },
+        url: {
+          type: 'string',
+          index: 'not_analyzed',
+        },
+        fda_approval: {
+          properties: {
+            action_date: {
+              type: 'date',
+              format: 'dateOptionalTime',
+            },
+            id: {
+              type: 'string',
+              index: 'not_analyzed',
+            },
+            notes: {
+              type: 'string',
+            },
+            supplement_number: {
+              type: 'long',
+            },
+            type: {
+              type: 'string',
+              index: 'not_analyzed',
+            },
+            fda_application: {
+              properties: {
+                id: {
+                  type: 'string',
+                  index: 'not_analyzed',
+                },
+                type: {
+                  type: 'string',
+                  index: 'not_analyzed',
+                },
+                url: {
+                  type: 'string',
+                  index: 'not_analyzed',
+                },
+                drug_name: {
+                  type: 'string',
+                },
+                active_ingredients: {
+                  type: 'string',
+                },
+              },
+            },
+          },
+        },
+        file: {
+          properties: {
+            id: {
+              type: 'string',
+              index: 'not_analyzed',
+            },
+            documentcloud_id: {
+              type: 'string',
+              index: 'not_analyzed',
+            },
+            sha1: {
+              type: 'string',
+              index: 'not_analyzed',
+            },
+            source_url: {
+              type: 'string',
+              index: 'not_analyzed',
+            },
+          },
+        },
+      },
+    },
     discrepancies: {
       properties: {
         public_title: getDiscrepancyRecordMapping({
