132d587c540ab50f8415f3c4943c70990c0204eb
Georgiana Bere <bere.georgi@gmail.com>
2016-12-01 00:24:53 +0200

    [#575] Remove stats endpoint
    opentrials/opentrials#575

--- api/models/source.js
+++ api/models/source.js
@@ -14,29 +14,6 @@ const Source = BaseModel.extend({
     'terms_and_conditions_url',
     'type',
   ],
-  latestUpdatedDates: function () {
-    return bookshelf.knex
-      .select(
-        bookshelf.knex.raw('source_id as id'),
-        bookshelf.knex.raw('sources.name as name'),
-        bookshelf.knex.raw('max(records.updated_at) as latest_updated_date')
-      )
-      .from('records')
-      .leftJoin('sources', 'source_id', 'sources.id')
-      .groupBy('source_id', 'sources.name');
-  },
-  trialsPerSource: function () {
-    return bookshelf.knex
-      .select(
-        bookshelf.knex.raw('source_id as id'),
-        bookshelf.knex.raw('sources.name as name'),
-        bookshelf.knex.raw('count(distinct records.trial_id)::int')
-      )
-      .from('records')
-      .leftJoin('sources', 'source_id', 'sources.id')
-      .groupBy('source_id', 'sources.name')
-      .orderBy('sources.name');
-  },
 });
 
 module.exports = bookshelf.model('Source', Source);
