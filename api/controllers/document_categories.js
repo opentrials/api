9f984fd619f6ad36f7379e26114b9b93430c4a17
Victor Nițu <victor@thesponge.eu>
2017-02-23 17:24:16 +0200

    Remove endpoint for document category by ID (revert this if needed)

--- api/controllers/document_categories.js
+++ api/controllers/document_categories.js
@@ -2,24 +2,6 @@
 
 const DocumentCategory = require('../models/document_category');
 
-function getDocumentCategory(req, res) {
-  const id = req.swagger.params.id.value;
-
-  return new DocumentCategory({ id }).fetch()
-    .then((doc) => {
-      if (doc) {
-        res.json(doc);
-      } else {
-        res.status(404);
-        res.finish();
-      }
-    })
-    .catch((err) => {
-      res.finish();
-      throw err;
-    });
-}
-
 function listDocumentCategories(req, res) {
   return DocumentCategory.fetchAll()
     .then((categories) => {
@@ -36,6 +18,5 @@ function listDocumentCategories(req, res) {
 }
 
 module.exports = {
-  getDocumentCategory,
   listDocumentCategories,
 };
--- test/api/controllers/document_categories.js
+++ test/api/controllers/document_categories.js
@@ -9,32 +9,6 @@ describe('Document category', () => {
 
   afterEach(clearDB);
 
-  describe('GET /v1/document_categories/{id}', () => {
-    it('returns 404 if there\'s no document with the received ID', () => (
-      server.inject('/v1/document_categories/0')
-        .then((response) => {
-          response.statusCode.should.equal(404);
-        })
-    ));
-
-    it('returns a document category', () => {
-      let cat;
-
-      return factory.create('document_category')
-        .then((_cat) => new DocumentCategory({ id: _cat.attributes.id }).fetch())
-        .then((_cat) => (cat = _cat))
-        .then(() => server.inject(`/v1/document_categories/${cat.attributes.id}`))
-        .then((response) => {
-          response.statusCode.should.equal(200);
-
-          const expectedResult = cat.toJSON();
-          const result = JSON.parse(response.result);
-
-          result.should.deepEqual(expectedResult);
-        });
-    });
-  });
-
   describe('GET /v1/document_categories', () => {
     it('returns the document categories', () => {
       let cat;
