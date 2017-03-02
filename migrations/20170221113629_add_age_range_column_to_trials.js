545aba6783db67f5ac680835cdcc3c31d5658fcb
Arthur Sena Lins Caldas <arthur.senaufcg@gmail.com>
2017-03-02 11:33:02 -0300

    Lints code

--- migrations/20170221113629_add_age_range_column_to_trials.js
+++ migrations/20170221113629_add_age_range_column_to_trials.js
@@ -1,20 +1,15 @@
+'use strict';
 
-exports.up = function(knex, Promise) {
-  const schema = knex.schema;
+exports.up = (knex) => (
+  knex.schema
+    .table('trials', (table) => {
+      table.jsonb('age_range');
+    })
+);
 
-  schema.table('trials', (table) => {
-    table.jsonb('age_range');
-  });
-
-  return schema;
-};
-
-exports.down = function(knex, Promise) {
-   const schema = knex.schema;
-
-  schema.table('trials', (table) => {
-    table.dropColumn('age_range');
-  });
-
-  return schema;
-};
+exports.down = (knex) => (
+  knex.schema
+    .table('trials', (table) => {
+      table.dropColumn('age_range');
+    })
+);
