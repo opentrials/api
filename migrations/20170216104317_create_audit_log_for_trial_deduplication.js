66893569ec035d25eb4334632815ca101e1e4851
Vitor Baptista <vitor@vitorbaptista.com>
2017-04-04 10:14:38 +0100

    [#661] Fix migration

--- migrations/20170216104317_create_audit_log_for_trial_deduplication.js
+++ migrations/20170216104317_create_audit_log_for_trial_deduplication.js
@@ -1,20 +1,29 @@
 'use strict';
 
-exports.up = (knex) =>
-  knex.schema.createTable('trial_deduplication_logs', (table) => {
-    table.increments();
-    table.uuid('record_id')
-      .references('records.id');
-    table.uuid('trial_id')
-      .references('trials.id');
-    table.text('method')
-      .notNullable();
-    table.text('commit')
-      .notNullable();
-    table.timestamps(true, true);
-    })
-;
+exports.up = (knex) => (
+  knex.schema
+    .createTable('trial_deduplication_logs', (table) => {
+      table.increments();
 
-exports.down = (knex) =>
+      table.uuid('record_id')
+        .references('records.id')
+        .notNullable();
+      table.uuid('trial_id')
+        .references('trials.id')
+        .notNullable();
+      table.text('method')
+        .notNullable();
+      table.text('commit');
+
+      table.timestamps(true, true);
+    })
+    .raw(`
+        CREATE TRIGGER trial_deduplication_logs_set_updated_at
+        BEFORE UPDATE ON trial_deduplication_logs
+        FOR EACH ROW EXECUTE PROCEDURE set_updated_at()
+    `)
+);
+
+exports.down = (knex) => (
   knex.schema.dropTableIfExists('trial_deduplication_logs')
-;
+);
